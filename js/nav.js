e0764a0e6df9fd043df36acc9d6a01ac0494d6ce
Spencer <backmanspencer99@gmail.com>
2019-04-02 12:33:47 -0500

    first commit

---
+++ js/nav.js
@@ -0,0 +1,35 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+let toggle = document.getElementsByClassName('nav-links');
+let home = document.getElementById('homeLink');
+let about = document.getElementById('aboutLink');
+let contact = document.getElementById('contactLink');
+let signs = document.getElementById('signsLink');
+let open = document.getElementById('open');
+let close = document.getElementById('close');
+
+navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle("active");
+    open.classList.remove("showOpen");
+    close.classList.toggle("showClose")
+})
+
+home.addEventListener("click", function() {
+    mainNav.classList.remove("active");
+    close.classList.remove("showClose");
+})
+
+about.addEventListener("click", function() {
+    mainNav.classList.remove("active");
+    close.classList.remove("showClose");
+})
+
+contact.addEventListener("click", function() {
+    mainNav.classList.remove("active");
+    close.classList.remove("showClose");
+})
+
+signs.addEventListener("click", function() {
+    mainNav.classList.remove("active");
+    close.classList.remove("showClose");
+})
